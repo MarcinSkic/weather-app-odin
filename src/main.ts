2b21c213ad926536a4d66639917370aebb55cab0
Marcin Skic <marcinskicoficjal@gmail.com>
2023-04-05 17:33:26 +0200

    Add form and fetch data into console. Start styling page

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,21 @@
-import "./style.css";
-import "./test.scss";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter";
+import "./style.scss";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+const form = document.querySelector<HTMLFormElement>("form")!;
+const searchBar = document.querySelector<HTMLInputElement>("input#location")!;
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+form.addEventListener("submit", (event: Event) => {
+    getData(searchBar.value);
+    event.preventDefault();
+});
+
+async function getData(query: String) {
+    const response = await fetch(
+        `http://api.weatherapi.com/v1/current.json?key=${
+            import.meta.env.VITE_WEATHER_API_KEY
+        }&q=${query}`
+    );
+    const data = await response.json();
+    console.log(data);
+}
+
+getData("auto:ip");
